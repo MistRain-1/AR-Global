77652a209e340dfbb9461be1c84dc3d42f4894fc
PC-20201002IMTW\Administrator <401941971@qq.com>
2020-11-06 14:12:37 +0800

    加一堆代码和一个地球

---
+++ AR-Test1/Assets/Script/StopToCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopToCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Global;
+    public GameObject Camera;
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void CatchGlobal()
+    {
+        Global.transform.SetParent(Camera.transform);
+    }
+}
---
+++ AR-Test1/Assets/Script/test.cs
@@ -0,0 +1,21 @@
+﻿using easyar;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame updatea
+    CameraDevice cameradevice;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
